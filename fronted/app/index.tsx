54a54628d7fd8ab6500a6ebde54ebc00b3444bc2
Shon Azenilok <shon8000@gmail.com>
2025-11-02 19:28:15 +0200

    updated home

--- fronted/app/index.tsx
+++ fronted/app/index.tsx
@@ -3,16 +3,26 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
-      <Text>Home</Text>
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Wisebuy</Text>
+      
+      <Text>shop smarter today</Text>
+      
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex : 1,
+        alignItems: 'center',
+        justifyContent : 'center'
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 18
+    }
+})
